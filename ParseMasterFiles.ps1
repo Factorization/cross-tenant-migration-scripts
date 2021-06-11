b18fb340a7b5882db789190fe5beb2ffc0eb9f63
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2021-06-11 10:00:01 -0700

    Added script

---
+++ ParseMasterFiles.ps1
@@ -0,0 +1,58 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)]
+    [string]
+    $Path
+)
+BEGIN {
+    $DOMAINS = @{
+        CDFA = @('cdfa.ca.gov', 'cdfao365.onmicrosoft.com')
+        CDPH = @('cdph.ca.gov', 'cdph.onmicrosoft.com')
+        DCA  = @('dca.ca.gov', 'dcao365.onmicrosoft.com')
+    }
+}
+PROCESS {
+    $Master_files = Get-ChildItem -Path $Path -File -Filter "Master_*.csv" | Where-Object { $_.name -notlike "*original*" }
+
+    $Mailbox_Types = @(
+        'Equipment',
+        'Room',
+        'Shared',
+        'User'
+    )
+
+    $Results = @{
+        CDFA = @{
+            Equipment = @()
+            Room      = @()
+            Shared    = @()
+            User      = @()
+        }
+        CDPH = @{
+            Equipment = @()
+            Room      = @()
+            Shared    = @()
+            User      = @()
+        }
+        DCA  = @{
+            Equipment = @()
+            Room      = @()
+            Shared    = @()
+            User      = @()
+        }
+    }
+
+    foreach ($T in $Mailbox_Types) {
+        $File = $Master_files | Where-Object { $_.Name -like "*$T*" }
+        $CSV = Import-Csv $File.FullName
+
+        foreach ($A in @("CDFA", "CDPH", "DCA")){
+            $Tenant_Domains = $DOMAINS[$A]
+
+            $Result = $CSV | Where-Object {$_.OldUPN -like "*$($Tenant_Domains[0])" -or $_.OldUPN -like "*$($Tenant_Domains[1])"}
+            $Results[$A][$T] = $Result
+        }
+    }
+    return $Results
+}
+END {}
