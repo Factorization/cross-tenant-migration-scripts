e113a1d3d30264d2480127c6586122fb1a704afd
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2021-06-28 08:59:48 -0700

    Added script to setup forwarding.

---
+++ SetForwardingAddress.ps1
@@ -0,0 +1,78 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $InputFile
+)
+BEGIN {
+    If (-not (Test-Path $InputFile)) {
+        Write-Host "File $InputFile does not exist. Exiting." -ForegroundColor Red
+        exit
+    }
+    If (-not $InputFile.EndsWith('.xlsx')) {
+        Write-Host "File $InputFile does not end with '.xlsx'. Exiting." -ForegroundColor Red
+        exit
+    }
+
+    $User_Mailboxes = Import-Excel $InputFile -WorksheetName "User Mailboxes"
+    $Shared_Mailboxes = Import-Excel $InputFile -WorksheetName "Shared Mailboxes"
+    $DATE = Get-Date -Format yyyy-MM-dd_HH.mm
+    $ErrorFile = "Email_Forwarding_Address_Errors_$DATE.csv"
+
+    function SetForwardingAddresses($Mailboxes) {
+        $Total = $Mailboxes | Measure-Object | Select-Object -ExpandProperty Count
+        $i = 0
+        $ErrorCount = 0
+        $ErrorList = @()
+        foreach ($User in $Mailboxes) {
+            Write-Progress -Id 1 -Activity "Setting forwarding addresses..." -Status "Mailboxes: [$i/$Total] | Errors: $ErrorCount" -PercentComplete ($i / $Total * 100)
+            $i++
+
+            $Source_Email = $User."Source Mailbox"
+            $Target_Email = $User."Target Mailbox"
+
+            Try {
+                $Mailbox = Get-Mailbox $Source_Email
+                if (($Mailbox | Measure-Object | Select-Object -ExpandProperty Count) -ne 1) {
+                    Throw "Invalid mailbox count."
+                }
+            }
+            Catch {
+                $err = $_
+                $User | Add-Member -MemberType NoteProperty -Name Error -Value $err
+                Write-Host "Error getting mailbox $Source_Email. Error: $err" -ForegroundColor Red
+                $ErrorCount += 1
+                $ErrorList += $User
+                Continue
+            }
+
+            Try {
+                # Set-Mailbox $Source_Email -DeliverToMailboxAndForward $true -ForwardingSMTPAddress $Target_Email
+                Start-Sleep -Seconds 1
+                Write-Verbose "Set forwarding address for $Source_Email to $Target_Email"
+            }
+            Catch {
+                $err = $_
+                $User | Add-Member -MemberType NoteProperty -Name Error -Value $err
+                Write-Host "Error getting mailbox $Source_Email. Error: $err" -ForegroundColor Red
+                $ErrorCount += 1
+                $ErrorList += $User
+                Continue
+            }
+        }
+        if ($ErrorList) {
+            $ErrorList | Export-Csv -NoTypeInformation -Append -LiteralPath $ErrorFile
+        }
+    }
+
+}
+PROCESS {
+
+    Write-Verbose "Working on user mailboxes..."
+    SetForwardingAddresses -Mailboxes $User_Mailboxes
+
+    Write-Verbose "Working on shared mailboxes..."
+    SetForwardingAddresses -Mailboxes $Shared_Mailboxes
+
+}
+END {}
