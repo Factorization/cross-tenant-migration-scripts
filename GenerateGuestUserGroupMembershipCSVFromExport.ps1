9856cee4d96dbc74515c184c204c985049443c76
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2022-02-15 16:47:25 -0800

    Updated username

--- GenerateGuestUserGroupMembershipCSVFromExport.ps1
+++ GenerateGuestUserGroupMembershipCSVFromExport.ps1
@@ -10,12 +10,16 @@ param (
 )
 BEGIN {
     $XML_Directory = Join-Path $exportPath "Guest_Azure_AD_User_Group_Membership_Output_XMLs"
+    $User_XML_Directory = Join-Path $exportPath "Guest_Azure_AD_User_Output_XMLs"
     $XMLs = Get-ChildItem -LiteralPath $XML_Directory -File *.xml
+    $User_XMLs = Get-ChildItem -LiteralPath $User_XML_Directory -File *.xml
 }
 PROCESS {
     $Results = @()
     foreach ($XML in $XMLs) {
-        $User = ($XML.basename -split "#EXT#")[0]
+        $User_XML = $User_XMLs | Where-Object {$_.Name -eq $XML.Name}
+        $AzureADUser = Import-Clixml $User_XML
+        $User = $AzureADUser.Mail
         $IndexOfUnderscore = $User.LastIndexOf("_")
         $User = $User.remove($IndexOfUnderscore, 1).insert($IndexOfUnderscore, "@")
         $Groups = Import-Clixml $XML.FullName | Where-Object { $_.DisplayName -notin $ExcludeGroups }
