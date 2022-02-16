249a7b0e6c6d47bee1ecbe48ba7bb2f57720219a
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2022-02-15 16:33:41 -0800

    Added script to export guest group membership

---
+++ GenerateGuestUserGroupMembershipCSVFromExport.ps1
@@ -0,0 +1,32 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [String]
+    $ExportPath,
+
+    [Parameter(Mandatory = $false)]
+    [string[]]
+    $ExcludeGroups = @()
+)
+BEGIN {
+    $XML_Directory = Join-Path $exportPath "Guest_Azure_AD_User_Group_Membership_Output_XMLs"
+    $XMLs = Get-ChildItem -LiteralPath $XML_Directory -File *.xml
+}
+PROCESS {
+    $Results = @()
+    foreach ($XML in $XMLs) {
+        $User = ($XML.basename -split "#EXT#")[0]
+        $Groups = Import-Clixml $XML.FullName | Where-Object { $_.DisplayName -notin $ExcludeGroups }
+        foreach ($Group in $Groups) {
+            $Results += [PSCustomObject]@{
+                GuestUser = $User
+                GroupName = $Group.DisplayName
+                GroupEmail = $Group.Mail
+            }
+        }
+    }
+    $OutputFile = Join-Path $ExportPath "CSV_Exports\Guest_Users_Group_Membership.csv"
+    $Results | Export-Csv -NoTypeInformation -LiteralPath $OutputFile
+    Write-host "File exported to $OutputFile" -ForegroundColor Green
+}
+END {}
