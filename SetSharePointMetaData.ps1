5cadc15ee94ae0a3cf7e62177e7842fb9c971046
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2022-03-03 23:21:06 -0800

    Updated code

--- SetSharePointMetaData.ps1
+++ SetSharePointMetaData.ps1
@@ -29,15 +29,15 @@ BEGIN {
     $TermIds = Export-PnPTaxonomy -IncludeID
     Write-Host "DONE" -ForegroundColor Green
 
-    $CSV = Import-Csv -LiteralPath $CSVFile | select -First 1
+    $CSV = Import-Csv -LiteralPath $CSVFile
 }
-PROCESS{
+PROCESS {
 
     $ErrorList = @()
     $Total = $CSV | Measure-Object | Select-Object -ExpandProperty Count
     $i = 0
 
-    foreach ($Line in $CSV){
+    foreach ($Line in $CSV) {
         $RelativePath = $Line.RelativePath
         $VendorFullPath = $Line.VendorFullPath
 
@@ -47,46 +47,44 @@ PROCESS{
         Write-Progress -Activity "Adding metadata to files..." -Status "Files: [ $i / $Total ] | Errors: $($ErrorList | Measure-Object | Select-Object -ExpandProperty Count) | Current File: $RelativePath" -PercentComplete (($i / $Total) * 100)
         $i++
 
-        $line | out-host
-
-        if(-not $RelativePath){
+        if (-not $RelativePath) {
             $Line | Add-Member -MemberType NoteProperty -Name Error -Value "Relative Path can't be empty."
             $ErrorList += $Line
             $Line | Out-Host
             Continue
         }
 
-        if(-not $VendorFullPath){
+        if (-not $VendorFullPath) {
             $Line | Add-Member -MemberType NoteProperty -Name Error -Value "Vendor Path is empty. Skipping."
             $ErrorList += $Line
             $Line | Out-Host
             Continue
         }
-        Try{
+        Try {
             $File = Get-PnPFile -Url $RelativePath -AsListItem
         }
-        Catch{
+        Catch {
             $err = $_
             $Line | Add-Member -MemberType NoteProperty -Name Error -Value "Error getting PNP file. Error: $err"
             $ErrorList += $Line
             $Line | Out-Host
             Continue
         }
-        if(-not $File){
+        if (-not $File) {
             $Line | Add-Member -MemberType NoteProperty -Name Error -Value "Failed to find file. Skipping"
             $ErrorList += $Line
             $Line | Out-Host
             Continue
         }
         $VendorLabel = ($VendorFullPath -split "\|")[-1]
-        $TermID = $TermIds | Where-Object {$_ -like "*|$VendorLabel;#*"}
-        if(-not $TermId){
+        $TermID = $TermIds | Where-Object { $_ -like "*|$VendorLabel;#*" }
+        if (-not $TermId) {
             $Line | Add-Member -MemberType NoteProperty -Name Error -Value "Failed to find term ID. Skipping"
             $ErrorList += $Line
             $Line | Out-Host
             Continue
         }
-        if(($TermID | Measure-Object | Select-Object -ExpandProperty Count) -ne 1){
+        if (($TermID | Measure-Object | Select-Object -ExpandProperty Count) -ne 1) {
             $Line | Add-Member -MemberType NoteProperty -Name Error -Value "Found multiple term IDs. Skipping"
             $ErrorList += $Line
             $Line | Out-Host
@@ -94,10 +92,10 @@ PROCESS{
         }
         $TermID = ($TermId -split "#")[-1]
 
-        try{
-            Set-PnpListItem -List $DocumentLibrary -Identity $File.Id -Values @{"Vendor" = $TermID} | Out-Null
+        try {
+            Set-PnpListItem -List $DocumentLibrary -Identity $File.Id -Values @{"Vendor" = $TermID } | Out-Null
         }
-        Catch{
+        Catch {
             $err = $_
             $Line | Add-Member -MemberType NoteProperty -Name Error -Value "Error setting PNP list item. Error: $err"
             $ErrorList += $Line
@@ -106,8 +104,8 @@ PROCESS{
         }
     }
 }
-END{
-    if($ErrorList){
+END {
+    if ($ErrorList) {
         $Date = Get-Date -Format "yyyy-MM-dd_HH.mm"
         $ErrorFile = "SharePoint_MetaData_Update_Error_List_$Date.csv"
         $ErrorList | Export-Csv -NoTypeInformation -LiteralPath $ErrorFile
