78b893da6966d3e0574b422b74ba98fe9fd31930
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2022-03-17 12:55:12 -0700

    added script

---
+++ GetSharePointSites.ps1
@@ -0,0 +1,58 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $OutputFile
+)
+BEGIN {
+    Write-Host "Getting SharePoint sites..." -ForegroundColor Cyan
+    $Sites = Get-SPOSite -Limit All | Where-Object { $_.Title } | Sort-Object Title
+}
+PROCESS {
+    $Total = $Sites | Measure-Object | Select-Object -ExpandProperty Count
+    $i = 0
+    $Results = @()
+    foreach ($Site in $Sites) {
+        Write-Progress -Activity "Getting SharePoint site stats..." -Status "[ $i / $Total ] | Site: $($Site.Title)" -PercentComplete (($i / $Total) * 100)
+        $i++
+        $Title = $Site.Title
+        $SizeMb = $Site.StorageUsageCurrent
+        $Template = $Site.Template
+        $GroupObjectID = $Site.GroupId.guid
+        if ($GroupObjectID -eq '00000000-0000-0000-0000-000000000000') {
+            $MemberCount = "n/a"
+            $Owner = $Site.Owner
+            $HasPlanner = $false
+        }
+        else {
+            $MemberCount = Get-UnifiedGroupLinks -Identity $GroupObjectID -LinkType Member | Measure-Object | Select-Object -ExpandProperty Count
+            $Owner = Get-UnifiedGroupLinks -Identity $GroupObjectID -LinkType Owner | Select-Object -ExpandProperty WindowsLiveID
+            $Planners = Get-PnPPlannerPlan -Group $GroupObjectID -ResolveIdentities
+            if ($Planners) {
+                $HasPlanner = $true
+            }
+            else {
+                $HasPlanner = $false
+            }
+        }
+        $IsTeamsConnected = $Site.IsTeamsConnected
+
+        $Results += [PSCustomObject]@{
+            Title                  = $Title
+            "Remove Don't Migrate" = $null
+            Org                    = $null
+            SizeMB                 = $SizeMb
+            Template               = $Template
+            "Is Teams Connected"   = $IsTeamsConnected
+            "Has Planner"          = $HasPlanner
+            "Member Count"         = $MemberCount
+            Owner                  = $Owner -join " | "
+        }
+    }
+}
+END {
+    if ($Results) {
+        $Results | Export-Excel -AutoSize -AutoFilter -FreezeTopRow -Path $OutputFile
+    }
+    Write-Host "File saved to: $OutputFile" -ForegroundColor Green
+}
