708c7e49322851fbf57639f10f4ea10166afc133
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2022-03-30 19:33:37 -0700

    More Updates

--- CreateSPandTeams.ps1
+++ CreateSPandTeams.ps1
@@ -8,37 +8,37 @@ param (
     [string]
     $OutputDir = $PSScriptRoot
 )
-BEGIN{
+BEGIN {
     $ErrorActionPreference = "stop"
     $CSV = Import-Csv $File
 }
-PROCESS{
+PROCESS {
     $Total = $CSV | Measure-Object | Select-Object -ExpandProperty Count
     $i = 0
     $Failed = @()
     $Success = @()
-    foreach ($Site in $CSV){
+    foreach ($Site in $CSV) {
         Write-Progress "Creating SharePoint and Teams sites..." -Status "Total: [$i / $Total] | Site: $($Site.TargetName)" -PercentComplete (($i / $Total) * 100)
         $i++
-        if($Site.Teams -eq "TRUE"){
-            try{
-                $NewTeam = New-Team -DisplayName $Site.TargetName -Visibility "Private"
+        if ($Site.Teams -eq "TRUE") {
+            try {
+                $NewTeam = New-Team -DisplayName $Site.TargetName -MailNickName $Site.TargetName -Visibility "Private"
                 $Site.TargetMailNickName = $NewTeam.MailNickName
                 $Success += $Site
             }
-            catch{
+            catch {
                 $err = $_
                 Write-Host "Failed to create Teams Site $($Site.TargetName). Error: $err" -ForegroundColor Red
                 $Failed += $Site
             }
         }
-        else{
-            try{
+        else {
+            try {
                 $NewSPSite = New-PNPSite -Type TeamSite -Title $Site.TargetName -Alias $Site.TargetName
                 $Site.TargetMailNickName = $Site.TargetName
                 $Success += $Site
             }
-            catch{
+            catch {
                 $err = $_
                 Write-Host "Failed to create SharePoint Site $($Site.TargetName). Error: $err" -ForegroundColor Red
                 $Failed += $Site
@@ -46,14 +46,14 @@ PROCESS{
         }
     }
 }
-END{
+END {
     $Date = Get-Date -Format yyyy-MM-dd_HH.mm
-    if($Success){
+    if ($Success) {
         $SuccessFile = Join-Path $OutputDir "SharePoint-And-Teams-Site-Created-$Date.csv"
         $Success | Export-Csv -NoTypeInformation $SuccessFile
         Write-Host "Created sites saved to CSV: '$SuccessFile'" -ForegroundColor Green
     }
-    if($Failed){
+    if ($Failed) {
         $FailedFile = Join-Path $OutputDir "Failed-SharePoint-And-Teams-Sites-$Date.csv"
         $Failed | Export-Csv -NoTypeInformation $FailedFile
         Write-Host "Failed sites saved to CSV: '$FailedFile'" -ForegroundColor Red
