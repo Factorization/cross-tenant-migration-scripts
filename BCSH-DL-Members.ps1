0f1cbc1844c21ec89d80272e6b0881ee7157686e
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2022-04-11 16:30:46 -0700

    Added script

---
+++ BCSH-DL-Members.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [string[]]
+    $ExcludedGroups = @()
+)
+BEGIN {
+    $DLs = Get-DistributionGroup | Sort-Object Name
+}
+PROCESS {
+    foreach ($DL in $DLs) {
+        $DisplayName = $Dl.DisplayName
+        $EmailAddress = $DL.PrimarySMTPAddress
+        if ($_.RecipientTypeDetails -eq 'MailUniversalSecurityGroup') {
+            $GroupType = "Mail-enabled security"
+        }
+        else {
+            $GroupType = "Distribution List"
+        }
+        $Members = Get-DistributionGroupMember $EmailAddress | Where-Object PrimarySMTPAddress | Sort-Object PrimarySMTPAddress | Where-Object Name -NotIn $ExcludedGroups
+        if (-not $Members) {
+            [PSCustomObject]@{
+                DisplayName  = $DisplayName
+                EmailAddress = $EmailAddress
+                GroupType    = $GroupType
+                Member       = "No Members"
+            }
+        }
+        else {
+            foreach ($Member in $Members) {
+                [PSCustomObject]@{
+                    DisplayName  = $DisplayName
+                    EmailAddress = $EmailAddress
+                    GroupType    = $GroupType
+                    Member       = $Member.PrimarySMTPAddress
+                }
+            }
+        }
+    }
+}
